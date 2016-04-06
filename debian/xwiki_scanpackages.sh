96fe702dd88e86a69009402e56b569e83c7ded34
Thomas Mortagne <thomas.mortagne@gmail.com>
2016-04-06 12:42:30 +0200

    Make stable regex more strict

--- debian/xwiki_scanpackages.sh
+++ debian/xwiki_scanpackages.sh
@@ -91,7 +91,7 @@ function update_repository ()
 update_repository $LTS_REP ".*-${LTS_BRANCH}\(\.[0-9]+\)*.deb"
 
 ## stable
-update_repository $STABLE_REP ".*[\.-][0-9]+.deb"
+update_repository $STABLE_REP ".*\.[0-9]+.deb"
 
 ## releases
 if [ -d $RELEASES_REP ]; then
