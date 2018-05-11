8433be84bb6128612982d10caee96e3fcc36ea01
Adel Atallah <2213999+atallahade@users.noreply.github.com>
2018-05-11 10:57:58 +0200

    [Misc] Weblate components generator script handles repository urls

--- weblate-scripts/generate_components.py
+++ weblate-scripts/generate_components.py
@@ -23,8 +23,18 @@ import json
 import os
 import sys
 
-def generate_component(name, slug, path, pre_commit_script, post_commit_script):
+def generate_component(line, pre_commit_script, post_commit_script, post_update_script):
     """Creates a component based on the given parameters"""
+    name, path, repo_url = line.rsplit(';', 2)
+    name, path, repo_url = name.strip(), path.strip(), repo_url.strip()
+    slug = name.lower().replace(' ', '-').replace('.', '-')
+    if repo_url in repo_urls:
+        repo_url = repo_urls[repo_url]
+        push_url = ''
+        post_update_script = ''
+    else:
+        repo_urls[repo_url] = 'weblate://{}/{}'.format(project, slug)
+        push_url = repo_url
     basename, extension = path.rsplit('.', 1)
     filemask = '.translation/' + basename + '_*.properties'
     template = '.translation/' + basename + '_en.properties'
@@ -40,12 +50,15 @@ def generate_component(name, slug, path, pre_commit_script, post_commit_script):
         "name": name,
         "slug": slug,
         "vcs": "github",
+        "repo": repo_url,
+        "push": push_url,
         "filemask": filemask,
         "template": template,
         "file_format": file_format,
         "extra_commit_file": extra_commit_file,
         "pre_commit_script": pre_commit_script,
         "post_commit_script": post_commit_script,
+        "post_update_script": post_update_script,
         "commit_message": "Translated using Weblate (%(language_name)s)\n"
                           "Currently translated at %(translated_percent)s%% "
                           "(%(translated)s of %(total)s strings)\n"
@@ -59,22 +72,22 @@ def generate_component(name, slug, path, pre_commit_script, post_commit_script):
 
 if __name__ == '__main__':
     DIRECTORY = os.getcwd()
-    PRE_COMMIT_SCRIPT = sys.argv[1] if len(sys.argv) > 2 else DIRECTORY + "/pre_commit.sh"
-    POST_COMMIT_SCRIPT = sys.argv[2] if len(sys.argv) > 3 else DIRECTORY + "/post_commit.sh"
+    PRE_COMMIT_SCRIPT = sys.argv[1] if len(sys.argv) > 1 else DIRECTORY + "/pre_commit.sh"
+    POST_COMMIT_SCRIPT = sys.argv[2] if len(sys.argv) > 2 else DIRECTORY + "/post_commit.sh"
+    POST_UPDATE_SCRIPT = sys.argv[3] if len(sys.argv) > 3 else DIRECTORY + "/post_update.sh"
 
     for file_name in os.listdir(DIRECTORY):
         if not fnmatch.fnmatch(file_name, "translation_list_*.txt"):
             continue
         start, end = len("translation_list_"), file_name.index(".txt")
-        component_name = file_name[start:end]
+        project = file_name[start:end]
         components = []
+        repo_urls = {}
         with open(file_name, 'r') as f:
             for line in f.read().splitlines():
-                name, path = line.rsplit(' ', 1)
-                name = name.strip()
-                slug = name.lower().replace(' ', '-').replace('.', '-')
-                components.append(generate_component(
-                    name, slug, path, PRE_COMMIT_SCRIPT, POST_COMMIT_SCRIPT
-                ))
-        with open("components_{}.json".format(component_name), "w+") as f:
+                if not line or line[0] == '#':
+                    continue
+                components.append(generate_component(line, PRE_COMMIT_SCRIPT,
+                                                     POST_COMMIT_SCRIPT, POST_UPDATE_SCRIPT))
+        with open("components_{}.json".format(project), "w+") as f:
             f.write(json.dumps(components))
