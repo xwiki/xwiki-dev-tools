ff3072de785eb549da4b8df2e771389780e09573
Adel Atallah <2213999+atallahade@users.noreply.github.com>
2018-05-29 14:52:01 +0200

    XINFRA-256: Automate translation merge between branches (master/LTS)
    * Fix translation file from a new module from being added

--- weblate-scripts/apply_translations.sh
+++ weblate-scripts/apply_translations.sh
@@ -3,6 +3,8 @@ FILES="`dirname "$0"`/translation_list_*.txt"
 PATHS=`awk -F';' 'NF && $0!~/^#/{print $2}' $FILES`
 
 for p in $PATHS; do
-  git checkout master -- "${p/.properties/_*.properties}"
-  git checkout master -- "${p/.xml/.*.xml}"
+  if [ -f $p ]; then
+    git checkout master -- "${p/.properties/_*.properties}"
+    git checkout master -- "${p/.xml/.*.xml}"
+  fi
 done
