0322f47aa8269fdf33a83e46ef569fe018e13ef1
Thomas Mortagne <thomas.mortagne@gmail.com>
2023-08-16 13:27:36 +0200

    Add support for use case where we only want to test platform without its parent

--- bash/set-branch-version.sh
+++ bash/set-branch-version.sh
@@ -6,7 +6,9 @@ BASE_VERSION=${CURRENT_VERSION%%-*}
 echo "Base version: $BASE_VERSION"
 NEW_VERSION=$BASE_VERSION-$(git branch --show-current)-SNAPSHOT
 echo "New version: $NEW_VERSION"
+STANDARD_VERSION=$BASE_VERSION-SNAPSHOT
+echo "Standard version: $STANDARD_VERSION"
 
 mvn -f pom.xml versions:set -DnewVersion=${NEW_VERSION} -DallowSnapshots=true -DgenerateBackupPoms=false -Plegacy,integration-tests,snapshot,docker
-mvn versions:update-parent -DallowSnapshots=true -DparentVersion=[${NEW_VERSION}] -DgenerateBackupPoms=false -N
+mvn versions:update-parent -DallowSnapshots=true -DparentVersion=[${STANDARD_VERSION}],[${NEW_VERSION}] -DgenerateBackupPoms=false -N
 sed -e  "s/<commons.version>.*<\/commons.version>/<commons.version>${NEW_VERSION}<\/commons.version>/" -i pom.xml || true
