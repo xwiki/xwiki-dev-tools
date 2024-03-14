43b0f9bf3b79d35d46b15dfae53f65fed104daee
Thomas Mortagne <thomas.mortagne@gmail.com>
2024-03-14 10:22:57 +0100

    [bash] Better handling of <platform.version> for xwiki-platform feature-deploy branches

--- bash/reset-branch-version.sh
+++ bash/reset-branch-version.sh
@@ -13,5 +13,12 @@ BASE_VERSION=${CURRENT_VERSION%%-*}
 echo "Base version: $BASE_VERSION"
 NEW_VERSION=$BASE_VERSION-SNAPSHOT
 echo "New version: $NEW_VERSION"
+REPOSITORY=$(basename -s .git `git config --get remote.origin.url`)
+echo "Repository: $REPOSITORY"
 
 find . -type f -name 'pom.xml' | xargs sed -i "s/$CURRENT_VERSION/$NEW_VERSION/g"
+
+if [[ $REPOSITORY == 'xwiki-platform' ]]
+then
+  sed -e "s/<platform.version>.*<\/platform.version>/<platform.version>\${commons.version}<\/platform.version>/" -i pom.xml
+fi
--- bash/set-branch-version.sh
+++ bash/set-branch-version.sh
@@ -15,7 +15,14 @@ NEW_VERSION=$BASE_VERSION-$(git branch --show-current)-SNAPSHOT
 echo "New version: $NEW_VERSION"
 STANDARD_VERSION=$BASE_VERSION-SNAPSHOT
 echo "Standard version: $STANDARD_VERSION"
+REPOSITORY=$(basename -s .git `git config --get remote.origin.url`)
+echo "Repository: $REPOSITORY"
 
 mvn versions:update-parent -DallowSnapshots=true -DparentVersion=[${STANDARD_VERSION}],[${NEW_VERSION}] -DgenerateBackupPoms=false -N
 mvn -f pom.xml versions:set -DnewVersion=${NEW_VERSION} -DallowSnapshots=true -DgenerateBackupPoms=false -Plegacy,integration-tests,snapshot,docker
-sed -e  "s/<commons.version>.*<\/commons.version>/<commons.version>${NEW_VERSION}<\/commons.version>/" -i pom.xml || true
+sed -e "s/<commons.version>.*<\/commons.version>/<commons.version>${NEW_VERSION}<\/commons.version>/" -i pom.xml
+
+if [[ $REPOSITORY == 'xwiki-platform' ]]
+then
+  sed -e "s/<platform.version>.*<\/platform.version>/<platform.version>${NEW_VERSION}<\/platform.version>/" -i pom.xml
+fi
