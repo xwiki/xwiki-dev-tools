2bd5904d5294b03cc06afa4e2221943075fb6301
Thomas Mortagne <thomas.mortagne@gmail.com>
2024-07-30 16:06:56 +0200

    XINFRA-418: Add a script to help with getting the SHA256 checksums of a remote Maven artifact from nexus * better error reporting

--- bash/mvn-sha256sum.sh
+++ bash/mvn-sha256sum.sh
@@ -1,13 +1,37 @@
-if [ $1 == 'h' ]
-then
+
+usage () {
   echo "Usage: set-branch-version.sh GROUPID:ARTIFACTID VERSION TYPE"
   echo "Print the SHA256 (256-bit) checksums of a Maven artifact accessible through http://nexus.xwiki.org/nexus/content/groups/public."
   echo ""
   echo "-h: Display this help."
   echo ""
   echo "Example: mvn-sha256sum.sh org.xwiki.platform:xwiki-platform-distribution-war 16.6.0 war"
-
+}
+if [ $1 == 'h' ]
+then
+  usage
   exit 0
+fi
+if [ -z $1 ]
+then
+  echo "Missing GROUPID:ARTIFACTID."
+
+  usage
+  exit 1
+fi
+if [ -z $2 ]
+then
+  echo "Missing VERSION."
+
+  usage
+  exit 1
+fi
+if [ -z $3 ]
+then
+  echo "Missing TYPE."
+
+  usage
+  exit 1
 fi
 id=$1
 groupid=${id%%:*}
