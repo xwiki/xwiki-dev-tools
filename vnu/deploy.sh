b700120b629dc39dd2addb932dad7aeca4dd167c
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-05-13 16:23:56 +0200

    [vnu] Fix the automatic deploy

--- vnu/deploy.sh
+++ vnu/deploy.sh
@@ -2,5 +2,11 @@
 wget https://github.com/validator/validator/releases/download/latest/vnu.jar -O vnu.jar
 VERSION_LINE=`unzip -p vnu.jar META-INF/MANIFEST.MF | grep Implementation-Version`
 VERSION=${VERSION_LINE##* }
+VERSION=`echo $VERSION | tr -d '\r'`
+echo "Setting the version $VERSION in the pom file"
+sed -i "s/<version>version<\/version>/<version>$VERSION<\/version>/g" pom.xml
 echo "Deploying version $VERSION"
-mvn deploy -Dfile=vnu.jar -DpomFile=pom.xml -Dversion=$VERSION
+mvn deploy:deploy-file -DrepositoryId=nexus.xwiki.org -Durl=https://nexus.xwiki.org/nexus/content/repositories/externals/ -Dfile=vnu.jar -DpomFile=pom.xml -Dversion=$VERSION
+echo "Restoring the pom file"
+git checkout pom.xml
+
