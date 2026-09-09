cc645c5e19df069499d7c7f4537f33205609ef69
Manuel Leduc <manuel.leduc@xwiki.com>
2026-09-09 13:49:30 +0200

    [Misc] Don't prefix contributor names with '*'
    * Output the contributor names as-is, so that callers can format them as they want
    * This is related to RN-70 where contributors are now structured data

--- xwiki-release-scripts/list_contributors.sh
+++ xwiki-release-scripts/list_contributors.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 SCRIPT_NAME=`basename "$0"`
 
+PROJECTS=("xwiki-commons" "xwiki-rendering" "xwiki-platform")
+
+## TODO: accept an optional prefix (e.g. "* ") to put in front of each contributor name, as a "-p" option.
+## Until then, callers can pipe the names through `sed '/./s/^/* /'`.
+usage() {
+  echo "Usage: $SCRIPT_NAME start_version end_version"
+  echo "Example: $SCRIPT_NAME 10.4 10.5"
+}
+
 VERSION1=$1
 VERSION2=$2
 
-PROJECTS=("xwiki-commons" "xwiki-rendering" "xwiki-platform")
-
 if [[ -z "$VERSION1" ]] || [[ -z "$VERSION2" ]]; then
-  echo "Usage: $SCRIPT_NAME start_version end_version"
-  echo "Example: $SCRIPT_NAME 10.4 10.5"
+  usage
   exit 1
 fi
 
@@ -52,10 +58,10 @@ for PROJECT in ${PROJECTS[@]}; do
   ## `only=true` means that we only take value for this trailers key (we ignore other possible trailers)
   ## `valueonly=true` means that we don't want to display the key
   ## `unfold=true` means that in case of multiple co-authors we display them all
-  ## The first sed command is there to remove email addresses
-  ## The second sed command is there to add a "* " prefix on each line
+  ## The sed command is there to remove email addresses
+  ## The awk command drops the empty lines
   ## The sort command is there to filter out multiple entries
-  git log --pretty=format:"%an %+(trailers:key=Co-authored-by,only=true,valueonly=true,unfold=true)" $FROM..$TO | sed -e 's#<.*>##' | sed -e 's/^\(.\)/* \1/' | sort -u >> $TMP_FILE
+  git log --pretty=format:"%an %+(trailers:key=Co-authored-by,only=true,valueonly=true,unfold=true)" $FROM..$TO | sed -e 's#<.*>##' | awk 'NF' | sort -u >> $TMP_FILE
   cd ..
 done
 
